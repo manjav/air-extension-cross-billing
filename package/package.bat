01f2502b87be3b32a0e1c89e05f020f00c66ca6f
Mansour Djawadi <mansurjavadi@gmail.com>
2017-03-27 23:48:50 +0430

    added package folder
    you can generate ane file with package.bat

---
+++ package/package.bat
@@ -0,0 +1,4 @@
+
+adt -package -storetype pkcs12 -keystore your_cert.p12 -storepass ****** -target ane iab-extension.ane extension.xml -swc Billing_extension_as3_lib.swc -platform Android-ARM -C android . -platform default -C default .
+
+
